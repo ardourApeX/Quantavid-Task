4577b768a59c56cb2157212f5acf3b134e2d594d
ardourApeX <joygupta501@gmail.com>
2021-09-26 01:31:45 +0530

    Style : Added UI to upload files

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import { Dashboard } from "./components";
 import { Login, Signup } from "./pages";
 import { Route, Routes } from "react-router-dom";
 import { PrivateRoute } from "./components/PrivateRoute/PrivateRoute";
+import logo from "./images/logo.png";
+import { ToggleLogin } from "./components/ToggleLogin/ToggleLogin";
 function App() {
 	return (
 		<div className="App">
+			<nav className="navbar">
+				<img className="logo" src={logo} alt="Logo" />
+				<ToggleLogin />
+			</nav>
 			<Routes>
-				<PrivateRoute path="/">
+				<Route path="/">
 					<Dashboard />
-				</PrivateRoute>
+				</Route>
 				<Route path="/login">
 					<Login />
 				</Route>
--- src/components/Dashboard/dashboard.jsx
+++ src/components/Dashboard/dashboard.jsx
@@ -1,14 +1,26 @@
 import "./dashboard.css";
-import logo from "../../images/logo.png";
-import { ToggleLogin } from "../ToggleLogin/ToggleLogin";
+import { IoIosFolderOpen } from "react-icons/io";
 export default function Dashboard() {
 	return (
 		<div className="dashboard-parent">
-			<div>
-				<nav className="navbar">
-					<img className="logo" src={logo} alt="Logo" />
-					<ToggleLogin />
-				</nav>
+			<div className="upload-section">
+				<h1>Upload your posts here</h1>
+				<span>File should be .mp4, .wap, .jpg, .jpeg </span>
+				<div className="upload">
+					<label class="custom-file-upload">
+						<input style={{ display: "none" }} type="file" />
+						<div className="add-file-button">
+							<IoIosFolderOpen className="upload-icon" />
+						</div>
+					</label>
+					<div className="file-action">
+						<h3>Uploaded File</h3>
+						<button disabled={false} className="upload-button">
+							Upload
+						</button>
+					</div>
+					<h3 className="file-name"></h3>
+				</div>
 			</div>
 		</div>
 	);
